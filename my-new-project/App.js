7556872e4aa3c430b88c2500189116623b69014e
melwin <melwin3210@gmail.com>
2024-10-07 14:10:09 +0530

    Added dynamic categories title on category screens

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -10,7 +10,7 @@ const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <>
-    <StatusBar style='dark'></StatusBar>
+    <StatusBar style='light'></StatusBar>
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerStyle:{backgroundColor:'#351401'}, headerTintColor:'white' ,contentStyle:{backgroundColor:'#3f2f25'}}} >
         <Stack.Screen name='MealsCategories' component={CategoriesScreen} options={{title:'All Categories'}} />
--- my-new-project/screens/MealsOverviewScreen.js
+++ my-new-project/screens/MealsOverviewScreen.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import {useLayoutEffect} from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native'
-import {MEALS} from "../data/dummy-data"
+import {CATEGORIES, MEALS} from "../data/dummy-data"
 import MealItem from './MealItem';
 
-const MealsOverviewScreen = ({route}) => {
+const MealsOverviewScreen = ({route, navigation}) => {
     const catId = route.params.categoryId;
 
     const displayedMeals = MEALS.filter((mealItem)=>{
         return mealItem.categoryIds.indexOf(catId) >= 0
-    })
+    });
+
+    useLayoutEffect(()=>{
+        const categoryTitle = CATEGORIES.find((category)=> category.id === catId).title;
+        navigation.setOptions({
+            title: categoryTitle,
+        });
+
+    },[catId, navigation])
 
 
 
